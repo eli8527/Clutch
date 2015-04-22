115321f435a672333519c661cd996eebf221c4bf
Eric Li <ericyoungli@gmail.com>
2015-04-22 17:41:32 -0400

    fixed nav, and added custom coupon support

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,19 @@
 angular.module('couponApp.controllers',[]).controller('CouponListController',['$scope','Coupon',function($scope,Coupon){
 
     Coupon.getAll().success(function(data){
-        $scope.items=data.results;
+        var coupons=data.results;
+        var custom = [];
+        var common = [];
+        for (i = 0; i < coupons.length; i++)
+        {
+            if (coupons[i].custom)
+                custom.push(coupons[i]);
+            else
+                common.push(coupons[i]);
+        }
+        
+        $scope.items = common;
+        $scope.custom = custom;
     });
 
 }]).controller('DetailController',['$scope','Coupon','$state','$stateParams',function($scope,Coupon,$state,$stateParams){
