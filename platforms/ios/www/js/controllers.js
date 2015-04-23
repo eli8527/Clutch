a255438c2f8b4a9f2cec55c4508037c3e0d3b943
Eric Li <ericyoungli@gmail.com>
2015-04-23 00:32:32 -0400

    custom coupon implementation
    - renamed app name in config.xml
    - added custom coupons
    - introduced time delays for custom coupons
    - added an info button/popup
    - used localStorage for persistence

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -1,24 +1,68 @@
-angular.module('couponApp.controllers',[]).controller('CouponListController',['$scope','Coupon',function($scope,Coupon){
+angular.module('couponApp.controllers', []).controller('CouponListController', ['$scope', 'Coupon', '$state', '$ionicPopup', function ($scope, Coupon, $state, $ionicPopup) {
+    var custom = localStorage.getItem("custom");
+    var common = localStorage.getItem("common");
 
-    Coupon.getAll().success(function(data){
-        var coupons=data.results;
-        var custom = [];
-        var common = [];
-        for (i = 0; i < coupons.length; i++)
-        {
-            if (coupons[i].custom)
-                custom.push(coupons[i]);
-            else
-                common.push(coupons[i]);
-        }
-        
-        $scope.items = common;
-        $scope.custom = custom;
-    });
-
-}]).controller('DetailController',['$scope','Coupon','$state','$stateParams',function($scope,Coupon,$state,$stateParams){
-
-    $scope.coupon={id:$stateParams.id,title:$stateParams.title,content:$stateParams.content};
+    if (custom == "0" && common == "0") {
+        Coupon.getAll().success(function (data) {
+            var coupons = data.results;
+            var cust = [];
+            var com = [];
 
 
-}]);;
+            // builds coupon lists
+            for (i = 0; i < coupons.length; i++) {
+                var coupon = coupons[i];
+
+                // checks if expired
+                if (Date.parse(coupon.expdate.iso) < new Date().getTime())
+                    continue;
+
+                if (coupon.custom) {
+                    setTimeout(addLater, coupon.tta, coupon);
+                } else
+                    com.push(coupon);
+            }
+
+            localStorage.setItem("custom", JSON.stringify(cust));
+            localStorage.setItem("common", JSON.stringify(com));
+            $state.go($state.current, {}, {
+                reload: true
+            });
+        });
+    }
+    $scope.items = JSON.parse(localStorage.getItem("common"));
+    $scope.custom = JSON.parse(localStorage.getItem("custom"));
+
+    // adds to custom coupon list
+    function addLater(coup) {
+        var custom = JSON.parse(localStorage.getItem("custom"));
+        custom.push(coup);
+        localStorage.setItem("custom", JSON.stringify(custom));
+        $state.go($state.current, {}, {
+            reload: true
+        });
+
+    }
+
+
+    $scope.showInfo = function () {
+        var alertPopup = $ionicPopup.alert({
+            title: 'Clutch',
+            template: 'Welcome to Clutch, a COS448 Project developed by Cissy Chen, Eric Li, Aditya Trivedi, and Julia Wang.'
+        });
+        alertPopup.then(function (res) {
+            
+        });
+    };
+
+}]).controller('DetailController', ['$scope', 'Coupon', '$state', '$stateParams', function ($scope, Coupon, $state, $stateParams) {
+
+    $scope.coupon = {
+        id: $stateParams.id,
+        title: $stateParams.title,
+        content: $stateParams.content,
+        expdate: $stateParams.expdate
+    };
+
+
+}]);
